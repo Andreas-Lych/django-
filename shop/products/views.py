79af04bbeac2cb572f7a170b0b57e4e23094d1cc
Andreas-Lych <caesar.a.1981@gmail.com>
2023-02-22 21:58:08 +0300

    upp gitignore

--- shop/products/views.py
+++ shop/products/views.py
@@ -1,4 +1,6 @@
 import logging
+
+from django.conf import settings
 from django.http import HttpResponse
 
 logger = logging.getLogger(__name__)
@@ -6,7 +8,8 @@ logger = logging.getLogger(__name__)
 
 def index(request):
     if request.GET.get("param"):
-        logger.error(f"My param = {request.GET.get('param')}")
+        logger.info(f"My custom var = {settings.MY_CUSTOM_VARIABLE}")
+        logger.info(f"My param = {request.GET.get('param')}")
     return HttpResponse("Shop index view")
 
 
