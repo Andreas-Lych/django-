49ee30341b19e62eb8103a2a8969300d86187163
Andreas-Lych <caesar.a.1981@gmail.com>
2023-03-29 21:12:55 +0300

    product.html is done

--- shop/tests/api/test_products.py
+++ shop/tests/api/test_products.py
@@ -1,5 +1,6 @@
 import pytest
 from rest_framework.test import APIClient
+from products.models import Product
 
 
 @pytest.mark.django_db
@@ -10,3 +11,23 @@ class TestProductApi:
     def test_my_index(self):
         response = self.client.get("/api/products/")
         assert response.status_code == 200
+        assert len(response.json()) == 0
+
+        response = self.client.post("/api/products/", data={
+            "title": "BMW",
+            "color": "WHITE",
+            "price": 275,
+        })
+        assert response.status_code == 201
+        assert Product.objects.exists()
+
+    def test_delete_product(self):
+        product = Product.objects.create(title="BMW", color="WHITE", price=275)
+
+        response = self.client.get(f"/api/products/{product.id}/")
+        assert response.status_code == 200
+        assert response.json()["title"] == "BMW"
+
+        response = self.client.delete(f"/api/products/{product.id}/")
+        assert response.status_code == 204
+        assert not Product.objects.exists()
